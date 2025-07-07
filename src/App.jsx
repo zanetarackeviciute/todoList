94df364ca4b2288e276c3b679e88058c050f76e5
Zaneta Rackeviciute <zaneta.rackevisiute@gmail.com>
2025-07-07 11:46:41 +0300

    Add todo form with input and list

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,27 @@
+import { useState } from "react"
 
+export default function App() {
+    const [newItem, setNewItem] = useState("")
+
+    return (    
+        <>  
+            <form onSubmit={handleSubmit} className="new-item-form">
+                <div className="form-row">
+                    <label htmlFor="item">New item</label>
+                    <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"/>
+                </div>
+                <button className="btn">Add</button>
+            </form>
+            <h1 className="header">Todo List</h1>
+            <ul>
+                <li>
+                    <label htmlFor="">
+                        <input type="checkbox" />
+                        Item 1
+                    </label>
+                    <button className="btn btn-delete">Delete</button>
+                </li>
+            </ul>
+        </>  
+    )
+}
