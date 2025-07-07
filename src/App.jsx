fa71edf22a15f9ef568326a2d0dac5d8eadfe60c
Zaneta Rackeviciute <zaneta.rackevisiute@gmail.com>
2025-07-07 13:09:16 +0300

    Add todo list rendering, toggle and delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,38 @@ import { useState } from "react"
 
 export default function App() {
     const [newItem, setNewItem] = useState("")
+    const [todos, setTodos] = useState([])
 
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        setTodos(currentTodos => {
+            return [
+                ...currentTodos,
+                { id: crypto.randomUUID(), title: newItem, completed: false},
+            ]
+        })
+
+        setNewItem('')
+    }
+
+    function toggleTodo(id, completed) {
+        setTodos(currentTodos => {
+            return currentTodos.map(todo => {
+                if (todo.id === id) {
+                    return {...todo, completed}
+                }
+
+            return todo
+            })
+        })
+    }
+
+    function deleteTodo(id) {
+        setTodos(currentTodos => {
+            return currentTodos.filter(todo => todo.id != id)
+        })
+    }
     return (    
         <>  
             <form onSubmit={handleSubmit} className="new-item-form">
@@ -14,13 +45,18 @@ export default function App() {
             </form>
             <h1 className="header">Todo List</h1>
             <ul>
-                <li>
-                    <label htmlFor="">
-                        <input type="checkbox" />
-                        Item 1
-                    </label>
-                    <button className="btn btn-delete">Delete</button>
-                </li>
+                {todos.length === 0 && 'No Todos'}
+                {todos.map(todo => {
+                    return <li key={todo.id}>
+                        <label>
+                        <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)}/>
+                            {todo.title}
+                        </label>
+                        <button onClick={() => deleteTodo(todo.id)} className="btn btn-delete">Delete</button>
+                    </li>
+                    
+                })}
+
             </ul>
         </>  
     )
